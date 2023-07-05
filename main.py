0f95f86c212a0cfb8a5c60df1037227186c35186
Aron Nick Santos <aronnicksnts@gmail.com>
2023-07-05 15:13:31 +0800

    added arguments to train multiple models at once

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ from argparse import ArgumentParser
 import json
 
 model_name = "Trial_2_UPAE"
-# Get JSON file from models/model_parameters/
-model_parameters = json.load(open(f'models/model_parameters/{model_name}.json'))
 
-# Set up the arguments for the model from the JSON file
-parser = ArgumentParser()
-for key, value in model_parameters.items():
-    parser.add_argument(f"--{key}", dest=key, type=type(value), default=value)
-    print("key: ", key, "value: ", value)
+# Create Argument parser accept multiple JSON Files
+json_files = ArgumentParser()
+json_files.add_argument('json_files', nargs='+')
 
-# Parse the arguments
-opt = parser.parse_args(args=[])
+args = json_files.parse_args()
+for model_name in args.json_files:
+    print(f"Training Model {model_name}")
+    # Get JSON file from models/model_parameters/
+    model_parameters = json.load(open(f'models/model_parameters/{model_name}.json'))
 
-uae_main.train(opt)
-uae_main.test_for_xray(opt, plot=True)
+    # Set up the arguments for the model from the JSON file
+    parser = ArgumentParser()
+    for key, value in model_parameters.items():
+        parser.add_argument(f"--{key}", dest=key, type=type(value), default=value)
+        print("key: ", key, "value: ", value)
+
+    # Parse the arguments
+    opt = parser.parse_args(args=[])
+
+    # Trains the model
+    uae_main.train(opt)
+    uae_main.test_for_xray(opt, plot=True, plot_name=model_name)
